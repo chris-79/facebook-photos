f45c2d443341d16f487e347d20c3932d66b03377
Chris Seymour <cseymour@uga.edu>
2013-04-25 16:03:58 -0400

    tweaking/simplifying

--- fb-photos.js
+++ fb-photos.js
@@ -4,22 +4,19 @@
 		return this.each(function() {
 			var albumID = settings.albumURL.replace(/.*(a\.)([0-9]+).*/,"$2");
 			var photoLimit = settings.limit||10;
-			var url = "https://graph.facebook.com/"+albumID+"/photos";
 			var target = $(this);
 			var imgStyle = settings.CSS||'';
 			var imgSizes = {'original':1,'huge':2,'x-large':3,'large':4,'medium':5,'small':6,'x-small':7};
 			var imgSize = imgSizes[settings.imgSize]||5;
-			$.getJSON(url, function success(result) {
-				target.html('');
-				var limit = settings.photoLimit;
-				if(result.data.length<limit) {
-					limit = result.data.length;
+			$.getJSON('https://graph.facebook.com/'+albumID+'/photos?callback=?',function(json,status,xhr){
+				if(json.data.length<photoLimit) {
+					photoLimit = json.data.length;
 				}
-				for(i=(result.data.length-1);i>=(result.data.length-limit);i--) {
-					var image = result.data[i]['images'][imgSize]['source'];
-					var altTxt = (result.data[i]['place']['name']==="") ? "Slideshow image" : "Photo from "+result.data[i]['place']['name'];
-					var img = "<img src='"+image+"' alt='"+altTxt+"' style='"+imgStyle+"'/>";
-					target.append(img);
+				target.html('');
+				for(i=(json.data.length-1);i>=(json.data.length-photoLimit);i--) {
+					var image = json.data[i].images[imgSize].source;
+					var altTxt = (json.data[i].place.name==="") ? "Slideshow image" : "Photo from "+json.data[i].place.name;
+					target.append("<img src='"+image+"' alt='"+altTxt+"' style='"+imgStyle+"'/>");
 				}
 			});
 		});
